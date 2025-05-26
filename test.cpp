2e505285088e09b0fefbd7d0f224eb5789611de1
Guppo207 <163645193+Guppo207@users.noreply.github.com>
2025-05-26 20:17:00 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,73 +4,80 @@
 #include <Account.h>
 #include <Transaction.h>
 
+using ::testing::_;
+using ::testing::Expectation;
+
 class MockAccount : public Account {
 public:
     MockAccount(int id, int balance) : Account(id, balance) {}
     
-    MOCK_METHOD(void, Unlock, (), (override));
     MOCK_METHOD(void, Lock, (), (override));
-    MOCK_METHOD(int, Id, (), (const, override));
+    MOCK_METHOD(void, Unlock, (), (override));
+    MOCK_METHOD(int, id, (), (const, override));
     MOCK_METHOD(void, ChangeBalance, (int diff), (override));
-    MOCK_METHOD(int, Balance, (), (const, override));
+    MOCK_METHOD(int, GetBalance, (), (override));
 };
 
 class MockTransaction : public Transaction {
 public:
     MOCK_METHOD(bool, Make, (Account& from, Account& to, int sum), (override));
-    MOCK_METHOD(void, SetFee, (int fee), (override));
-    MOCK_METHOD(int, Fee, (), (const, override));
+    MOCK_METHOD(void, set_fee, (int fee), (override));
+    MOCK_METHOD(int, fee, (), (override));
 };
 
-TEST(AccountTest, BalanceAndIdOperations) {
-    MockAccount account(1, 100);
-    
-    EXPECT_CALL(account, Balance()).Times(3);
-    EXPECT_CALL(account, Lock()).Times(1);
-    EXPECT_CALL(account, Unlock()).Times(1);
-    EXPECT_CALL(account, ChangeBalance(testing::_)).Times(2);
-    EXPECT_CALL(account, Id()).Times(1);
-    
-    account.Balance();
-    account.Id();
-    account.Unlock();
-    account.ChangeBalance(1000);
-    account.Balance();
-    account.ChangeBalance(2);
-    account.Balance();
-    account.Lock();
+namespace AccountTests {
+    TEST(AccountTest, ShouldHandleBalanceAndIdOperations) {
+        MockAccount account(1, 100);
+        
+        Expectation get_balance = EXPECT_CALL(account, GetBalance()).Times(3);
+        Expectation lock = EXPECT_CALL(account, Lock()).Times(1).After(get_balance);
+        Expectation unlock = EXPECT_CALL(account, Unlock()).Times(1);
+        EXPECT_CALL(account, ChangeBalance(_)).Times(2);
+        EXPECT_CALL(account, id()).Times(1);
+
+        account.GetBalance();
+        account.id();
+        account.Unlock();
+        account.ChangeBalance(1000);
+        account.GetBalance();
+        account.ChangeBalance(2);
+        account.GetBalance();
+        account.Lock();
+    }
+
+    TEST(AccountTest, ShouldThrowWhenNotLocked) {
+        Account account(0, 100);
+        
+        EXPECT_THROW(account.ChangeBalance(50), std::runtime_error);
+        
+        account.Lock();
+        account.ChangeBalance(50);
+        EXPECT_EQ(account.GetBalance(), 150);
+        
+        EXPECT_THROW(account.Lock(), std::runtime_error);
+        account.Unlock();
+    }
 }
 
-TEST(AccountTest, LockingMechanism) {
-    Account account(0, 100);
-    
-    EXPECT_THROW(account.ChangeBalance(50), std::runtime_error);
-    
-    account.Lock();
-    account.ChangeBalance(50);
-    EXPECT_EQ(account.Balance(), 150);
-    
-    EXPECT_THROW(account.Lock(), std::runtime_error);
-    account.Unlock();
-}
+namespace TransactionTests {
+    TEST(TransactionTest, ShouldHandleMoneyTransfer) {
+        MockTransaction transaction;
+        MockAccount account1(1, 100);
+        MockAccount account2(2, 250);
+        MockAccount flat_org(3, 10000);
+        MockAccount organization(4, 5000);
 
-TEST(TransactionTest, TransferOperations) {
-    MockTransaction transaction;
-    MockAccount first_account(1, 100);
-    MockAccount second_account(2, 250);
-    MockAccount flat_org(3, 10000);
-    MockAccount organization(4, 5000);
-    
-    EXPECT_CALL(transaction, SetFee(testing::_)).Times(1);
-    EXPECT_CALL(transaction, Fee()).Times(1);
-    EXPECT_CALL(transaction, Make(testing::_, testing::_, testing::_)).Times(2);
-    EXPECT_CALL(first_account, Balance()).Times(1);
-    EXPECT_CALL(second_account, Balance()).Times(1);
-    
-    transaction.SetFee(300);
-    transaction.Make(first_account, second_account, 2000);
-    transaction.Fee();
-    first_account.Balance();
-    second_account.Balance();
-    transaction.Make(organization, first_account, 1000);
+        EXPECT_CALL(transaction, set_fee(_)).Times(1);
+        EXPECT_CALL(transaction, fee()).Times(1);
+        EXPECT_CALL(transaction, Make(_, _, _)).Times(2);
+        EXPECT_CALL(account1, GetBalance()).Times(1);
+        EXPECT_CALL(account2, GetBalance()).Times(1);
+
+        transaction.set_fee(300);
+        transaction.Make(account1, account2, 2000);
+        transaction.fee();
+        account1.GetBalance();
+        account2.GetBalance();
+        transaction.Make(organization, account1, 1000);
+    }
 }
